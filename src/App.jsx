689d890fc06df95f44db6af351ba629956572885
Lalitha Gadde <lalithagadde0810@gmail.com>
2024-08-26 11:54:55 -0400

    Added static Customer Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
-  return ("Hello")
+  function handleDelete() {
+    console.log("in onDeleteClick()");
+  }
+
+  function handleSave() {
+    console.log("in onSaveClick()");
+  }
+
+  function handleCancel() {
+    console.log("in onCancelClick()");
+  }
+
+  return (
+    <>
+    <div>
+    <h2>Update</h2>
+    <form>
+      <div>
+        <label>
+          Name: 
+          <input type='text' name='name' placeholder='Customer name'></input>
+        </label>
+      </div>
+
+      <div>
+        <label>
+          Email: 
+          <input type='text' name='email' placeholder='Customer email'></input>
+        </label>
+      </div>
+
+      <div>
+        <label>
+          Password: 
+          <input type='text' name='password' placeholder='Customer password'></input>
+        </label>
+      </div>
+
+      <div>
+        <button type='button' onClick={() => handleDelete()}>Delete</button>
+        <button type='button' onClick={() => handleSave()}>Save</button>
+        <button type='button' onClick={() => handleCancel()}>Cancel</button>
+      </div>
+
+    </form>
+  </div>
+    </>
+  )
+  
 }
 
 export default App
