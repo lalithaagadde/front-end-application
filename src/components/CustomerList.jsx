31ca306f9ddea857e6e2303de29f109cece409fc
Lalitha Gadde <lalithagadde0810@gmail.com>
2024-08-28 17:00:23 -0400

    Added search feature

--- src/components/CustomerList.jsx
+++ src/components/CustomerList.jsx
@@ -2,9 +2,32 @@ import { useEffect, useState } from "react";
 
 export default function CustomerList(props){
   const customers = props.data;
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredList, setFilteredList] = useState(customers);
+
+  useEffect(() => {
+    setFilteredList(props.data); // Initialize with all customers
+}, [props.data]);
+
+  function handleSearch(e) {
+    setSearchQuery(e.target.value);
+    const preFilteredList = props.data.filter(customer => (
+      customer.name.toLowerCase().includes(searchQuery) ||
+      customer.email.toLowerCase().includes(searchQuery) 
+    ));
+
+    setFilteredList(preFilteredList);
+  }
+
 
     return(
         <div className='container-list'>
+          <input 
+          type='text' 
+          name='searchField' 
+          placeholder='Search by name or email...' 
+          onChange={handleSearch}
+          value = {searchQuery} />
           
           <table>
             <thead>
@@ -15,7 +38,7 @@ export default function CustomerList(props){
               </tr>
             </thead>
             <tbody>
-              {customers.map((customer, index) => (
+              {filteredList.map((customer, index) => (
                 <tr key={index} onClick={() => props.handleListClick(index, customer._id)} className={props.selectedRow === index ? 'selected-row' : ''}>
                   <td>{customer.name}</td>
                   <td>{customer.email}</td>
